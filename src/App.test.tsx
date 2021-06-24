6fe1bce752375fc0ca6b26dda256caee67a4fb1d
Vladyslav Bondarenko <vlad-bondarenko@ukr.net>
2021-06-24 19:31:31 +0300

    chore(task): tests for useForm hook

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders form', () => {
-  render(<App />);
-  const formElement = screen.getByTestId('form');
-  expect(formElement).toBeInTheDocument();
-});
